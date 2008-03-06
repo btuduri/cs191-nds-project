8b29095a1f441692b19f2e6a05e178e5ba63f119
kingofcode <kingofcode@67d8362a-e844-0410-8493-f333cf7aaa27>
2008-03-06 08:02:11 +0000

    -added a sprite version that has mario just standin there. -deleted a folder that had the same stuff as the main

---  cs191-nds-project/splash_screen/main.cpp
+++  cs191-nds-project/splash_screen/main.cpp
@@ -2,21 +2,50 @@
 #include <stdio.h>
 
 #include "cScreenManager.h"
+#include "menuButton.h"
 
+//Backgrounds
 #include "graphics/title.h"
 #include "graphics/splash.h"
 #include "graphics/menu0.h"
 #include "graphics/menu1.h"
 #include "graphics/menu2.h"
 #include "graphics/menu3.h"
-#include "menuButton.h"
+
+//Sprites
+#include "graphics/mario.h"
+
+tOAM sprites;
+
+//turn off all the sprites
+void initSprites(void)
+{
+	for(int i = 0; i < 128; i++)
+	{
+		sprites.spriteBuffer[i].attribute[0] = ATTR0_DISABLED;
+		sprites.spriteBuffer[i].attribute[1] = 0;
+		sprites.spriteBuffer[i].attribute[2] = 0;
+		sprites.spriteBuffer[i].attribute[3] = 0;
+	}
+}
+
+//copy our sprite to object attribute memory
+void updateOAM(void)
+{
+	//DC_FlushRange(sprites,128*sizeof(SpriteEntry));
+	DC_FlushRange(sprites.spriteBuffer, SPRITE_COUNT * sizeof(SpriteEntry));
+
+	//dmaCopy(sprites, OAM, 128 * sizeof(SpriteEntry));
+	dmaCopy(sprites.spriteBuffer, OAM, SPRITE_COUNT * sizeof(SpriteEntry));
+}
+
+
 
 void initVideo(cScreenManager *sm) {
 
-	sm->setScreenMode(MAIN, MODE5 | DISPLAY_BG2_ACTIVE, false);
+	sm->setScreenMode(MAIN, MODE5 | DISPLAY_BG2_ACTIVE | DISPLAY_SPR_ACTIVE | DISPLAY_SPR_1D, false);
 
-	sm->setScreenMode(SUB, MODE5 | DISPLAY_BG2_ACTIVE | DISPLAY_BG3_ACTIVE,
-			false);
+	sm->setScreenMode(SUB, MODE5 | DISPLAY_BG2_ACTIVE, false);
 
 	vramSetBankA(VRAM_A_MAIN_BG_0x06000000);
 	//vramSetBankB(VRAM_B_MAIN_BG_0x06020000);
@@ -53,48 +82,29 @@ void initBackground() {
 
 	SUB_BG3_CR = BG_BMP16_256x256 | BG_BMP_BASE(8) | BG_PRIORITY_2;
 
-	//	SUB_BG3_CR = BG_BMP16_256x256 | BG_BMP_BASE(8) | BG_PRIORITY_2;	
-	//	SUB_BG3_XDX = 1 << 8;
-	//	SUB_BG3_XDY = 0;
-	//	SUB_BG3_YDX = 0;
-	//	SUB_BG3_YDY = 1 << 8;
-	//
-	//	// Move the image
-	//	SUB_BG3_CX = 0;
-	//	SUB_BG3_CY = 0;
-	//
-	//		
-
-	/*	
-	 BG3_CR = BG_BMP16_256x256 | BG_BMP_BASE(8) | BG_PRIORITY_2;
-
-	 // Affine matrix
-	 //        | 1.0 0.0 |
-	 // BG3 =  | 0.0 1.0 |
-	 BG3_XDX = 1 << 8;
-	 BG3_XDY = 0;
-	 BG3_YDX = 0;
-	 BG3_YDY = 1 << 8;
-	 
-	 // Move the image
-	 BG3_CX = 0;
-	 BG3_CY = 0;
-	 // The bottom layer is Mt. Fuji
-	 BG2_CR = BG_BMP16_256x256 | BG_BMP_BASE(16) | BG_PRIORITY_3;
-
-	 // Affine matrix
-	 //        | 1.0 0.0 |
-	 // BG1 =  | 0.0 1.0 |
-	 BG2_XDX = 1 << 8;
-	 BG2_XDY = 0;
-	 BG2_YDX = 0;
-	 BG2_YDY = 1 << 8;
-	 
-	 // Move the image
-	 BG2_CX = 0;
-	 BG2_CY = 0;
-	 */
 }
+
+
+bool inc = true;
+int adder = 64*64;
+void AnimateMario(void)
+{
+	
+	
+	if(inc)
+	{
+		sprites.spriteBuffer[0].tileIdx = sprites.spriteBuffer[0].tileIdx + 128;
+		if(sprites.spriteBuffer[0].tileIdx == (128*6))
+			inc = false;
+	}
+	else
+	{
+		sprites.spriteBuffer[0].tileIdx = sprites.spriteBuffer[0].tileIdx - 128;
+		if(sprites.spriteBuffer[0].tileIdx == 0)
+			inc = true;
+	}
+}
+
 
 int main(void) {
 
@@ -110,6 +120,13 @@ int main(void) {
 	irqInit();
 	irqSet(IRQ_VBLANK, 0);
 
+	// Setup Timer 3 to generate pulses every 1/4 second.
+	TIMER3_DATA = TIMER_FREQ_1024(6);
+	TIMER3_CR = TIMER_DIV_1024 | TIMER_IRQ_REQ | TIMER_ENABLE;
+	
+	irqSet(IRQ_TIMER3, AnimateMario);
+	
+	
 	initVideo(sm);
 	initBackground();
 
@@ -126,9 +143,43 @@ int main(void) {
 		swiWaitForVBlank();
 	}
 
+    //turn off the sprites
+    initSprites();
+
+    // Setup VRAM_B for Sprite Binary data
+	vramSetBankB(VRAM_B_MAIN_SPRITE_0x06400000);
+	
+	// Numbered square sprite
+	sprites.spriteBuffer[0].colMode = OBJCOLOR_256;
+	sprites.spriteBuffer[0].objMode = OBJMODE_NORMAL;
+	sprites.spriteBuffer[0].objShape = OBJSHAPE_SQUARE;
+	sprites.spriteBuffer[0].objSize = OBJSIZE_64;
+	sprites.spriteBuffer[0].objPriority = OBJPRIORITY_0;
+	sprites.spriteBuffer[0].tileIdx = 0;
+	sprites.spriteBuffer[0].posX = 10;
+	sprites.spriteBuffer[0].posY = 120;
+	sprites.spriteBuffer[0].isHidden = false;
+
+	// Load the sprite palette.
+	dmaCopy(marioPalette, SPRITE_PALETTE, marioPaletteLength);
+	
+	// Load the sprite binary data into the VRAM
+	dmaCopy(marioBitmap, SPRITE_GFX, marioBitmapLength);
+	
+	sprites.matrixBuffer[0].hdx=256;
+	sprites.matrixBuffer[0].hdy=0;
+	sprites.matrixBuffer[0].vdx=0;
+	sprites.matrixBuffer[0].vdy=256;
+	
+	
 	int bg = 0;
 	// Copy the data from program memory to VRAM.
 	dmaCopy(menu0Bitmap, (u16*)BG_BMP_RAM(0), menu0BitmapLen);
+	
+	irqEnable(IRQ_VBLANK);
+	irqEnable(IRQ_TIMER3);
+
+	
 	bool key_changed = false;
 	while ( 1) {
 		touchXY = touchReadXY();
@@ -168,6 +219,7 @@ int main(void) {
 			}
 		}
 		swiWaitForVBlank();
+		updateOAM();
 		swiDelay(1500000);
 
 	}
